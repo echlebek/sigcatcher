9a1bbe66859b48ae9efcc027e058e01e35f8e22e
Eric Chlebek <eric@sensu.io>
2018-05-01 14:44:40 -0700

    Make sigcatcher a little less shitty.

--- main.go
+++ main.go
@@ -4,40 +4,59 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"time"
 )
 
+type sigHistory struct {
+	data []int64
+}
+
+func (s *sigHistory) Append(x int64) {
+	if len(s.data) == cap(s.data) {
+		copy(s.data, s.data[1:])
+		s.data[len(s.data)-1] = x
+	} else {
+		s.data = append(s.data, x)
+	}
+}
+
+func (s *sigHistory) Average() int64 {
+	var total int64
+	for _, v := range s.data {
+		total += v
+	}
+	return total / int64(len(s.data))
+}
+
 var (
-	signalCounter int64
-	prevSignals   int64
+	history    = sigHistory{data: make([]int64, 0, 4)}
+	sigCount   int64
+	totalCount int64
 )
 
 func main() {
 	ch := make(chan os.Signal, 1000000)
 	signal.Notify(ch)
 
-	ticker := time.NewTicker(250 * time.Millisecond)
+	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
 
 	for {
 		select {
 		case <-ticker.C:
-			nSignals := atomic.LoadInt64(&signalCounter)
-			window := nSignals - prevSignals
-			perSecond := (window - 4) * 4
-			if perSecond < 0 {
-				perSecond = 0
-			}
-			perSecond += window % 4
-			fmt.Printf("\r %d signals/s (%d total)", perSecond, nSignals)
-			prevSignals = nSignals
+			nSignals := sigCount
+			sigCount = 0
+			totalCount += nSignals
+			history.Append(nSignals)
+			perSecond := history.Average()
+			fmt.Printf("\r                                                                         ")
+			fmt.Printf("\r%d signals/s\t\t\t%d total signals", perSecond, totalCount)
 		case sig := <-ch:
 			switch sig {
 			case os.Interrupt:
 				return
 			default:
-				atomic.AddInt64(&signalCounter, 1)
+				sigCount++
 			}
 		}
 	}
